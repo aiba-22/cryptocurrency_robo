6cbe9a95a7abbf340eee408158d98ebb6e588d1f
aiba <jazzarubamu@gmail.com>
2024-07-07 21:34:01 +0900

    ルーティング付与

---
+++ src/components/page01.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page01() {
+  return (
+    <div>
+      <h1>Page 01</h1>
+      <p>Welcome to Page 01!</p>
+    </div>
+  );
+}
+
+export default Page01;
---
+++ src/components/page02.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page02() {
+  return (
+    <div>
+      <h1>Page 02</h1>
+      <p>Welcome to Page 01!</p>
+    </div>
+  );
+}
+
+export default Page02;
