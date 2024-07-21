f15039967a1aa03b464570144a0ef92d27b45cec
aiba <jazzarubamu@gmail.com>
2024-07-21 10:23:02 +0900

    セレクトボックス追加

--- frontend/src/components/page01.js
+++ frontend/src/components/page01.js
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 // APIリクエストを送信してレスポンスを処理する関数
-async function fetchCoincheckStatus(pair = 'etc_jpy') {
+async function fetchCoincheckStatus(pair = 'btc_jpy') {
   try {
     const response = await axios.get(`http://localhost:3001/api/ticker?pair=${pair}`);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
@@ -17,12 +16,18 @@ function Page01() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [pair, setPair] = useState('btc_jpy');
+
+  const pairs = [
+    'btc_jpy', 'etc_jpy', 'lsk_jpy', 'mona_jpy', 'plt_jpy',
+    'fnct_jpy', 'dai_jpy', 'wbtc_jpy', 'bril_jpy'
+  ];
 
   useEffect(() => {
-    // 非同期関数を呼び出してデータを取得
     const fetchData = async () => {
       try {
-        const result = await fetchCoincheckStatus();
+        setLoading(true);
+        const result = await fetchCoincheckStatus(pair);
         setData(result);
         setLoading(false);
       } catch (error) {
@@ -32,19 +37,24 @@ function Page01() {
     };
 
     fetchData();
-  }, []);
+  }, [pair]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const handleChange = (event) => {
+    setPair(event.target.value);
+  };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-return (
+  return (
     <div>
       <h1>Page01</h1>
+      <select value={pair} onChange={handleChange}>
+        {pairs.map((pair) => (
+          <option key={pair} value={pair}>
+            {pair.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
       {data && (
         <div>
           <p>Last: {data.last}</p>
