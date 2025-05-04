570dc68d94bb2d2a7150e9cba0f9802e4c44e22e
aiba <jazzarubamu@gmail.com>
2025-05-04 13:38:58 +0900

    例外発生時のハンドリング

--- api/src/app.ts
+++ api/src/app.ts
@@ -27,6 +27,9 @@ import {
   create as createCryptocurrencyOrder,
   update as updateCryptocurrencyOrder,
 } from "./controllers/cryptocurrencyOrderController";
+
+import { asyncHandler } from "./ utils/asyncHandler";
+
 const app = express();
 const port = 3001;
 
@@ -34,20 +37,39 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/api/line", getLine);
-app.post("/api/line", createLine);
-app.put("/api/line", updateLine);
-app.get("/api/priceAlert", getPriceAlert);
-app.post("/api/priceAlert", createPriceAlert);
-app.put("/api/priceAlert", updatePriceAlert);
-app.get("/api/gmo", getGmo);
-app.post("/api/gmo", createGmo);
-app.put("/api/gmo", updateGmo);
-app.get("/api/VirtualCurrencyRateList", getVirtualCurrencyRateList);
-app.post("/api/notification/line", lineNotification);
-app.get("/api/cryptocurrencyOrder/list", listCryptocurrencyOrder);
-app.post("/api/cryptocurrencyOrder", createCryptocurrencyOrder);
-app.put("/api/cryptocurrencyOrder", updateCryptocurrencyOrder);
+app.get("/api/line", asyncHandler(getLine));
+app.post("/api/line", asyncHandler(createLine));
+app.put("/api/line", asyncHandler(updateLine));
+
+app.get("/api/priceAlert", asyncHandler(getPriceAlert));
+app.post("/api/priceAlert", asyncHandler(createPriceAlert));
+app.put("/api/priceAlert", asyncHandler(updatePriceAlert));
+
+app.get("/api/gmo", asyncHandler(getGmo));
+app.post("/api/gmo", asyncHandler(createGmo));
+app.put("/api/gmo", asyncHandler(updateGmo));
+
+app.get(
+  "/api/VirtualCurrencyRateList",
+  asyncHandler(getVirtualCurrencyRateList)
+);
+
+app.post("/api/notification/line", asyncHandler(lineNotification));
+
+app.get("/api/cryptocurrencyOrder/list", asyncHandler(listCryptocurrencyOrder));
+app.post("/api/cryptocurrencyOrder", asyncHandler(createCryptocurrencyOrder));
+app.put("/api/cryptocurrencyOrder", asyncHandler(updateCryptocurrencyOrder));
+
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    res.status(500).json({ message: "システムエラー" });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Proxy server is running on http://localhost:${port}`);
