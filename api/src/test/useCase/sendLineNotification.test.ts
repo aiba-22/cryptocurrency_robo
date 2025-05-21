53d1a8881f3883b25d0ed38c58915b40c1ae2447
aiba <jazzarubamu@gmail.com>
2025-05-21 11:31:31 +0900

    UT対応

--- api/src/test/useCase/sendLineNotification.test.ts
+++ api/src/test/useCase/sendLineNotification.test.ts
@@ -2,8 +2,6 @@ import { sendTestNotification } from "../../useCase/sendLineNotification";
 import LineService from "../../service/lineService";
 import LineApiService from "../../service/lineApiService";
 
-jest.mock("../../service/gmoService");
-jest.mock("../../service/gmoApiService");
 jest.mock("../../service/lineService");
 jest.mock("../../service/lineApiService");
 
@@ -23,34 +21,24 @@ describe("sendTestNotification", () => {
     }));
   });
 
-  it("正常系: メッセージが送信される", async () => {
+  it("メッセージの送信結果を返す", async () => {
     mockLineServiceFind.mockResolvedValue({
       userId: "testUserId",
       channelAccessToken: "testChannelAccessToken",
     });
-
+    mockSendMessage.mockResolvedValue("success");
     await expect(sendTestNotification("testMessage")).resolves.toEqual(
       "success"
     );
     expect(mockSendMessage).toHaveBeenCalledWith("testMessage");
   });
 
-  it("異常系: Line設定がない場合", async () => {
+  it("Line設定がない場合systemErrorを返す", async () => {
     mockLineServiceFind.mockResolvedValue(null);
 
-    await expect(sendTestNotification("testMessage")).resolves.toBeUndefined();
-    expect(mockSendMessage).not.toHaveBeenCalled();
-  });
-
-  it("異常系: メッセージ送信に失敗した場合", async () => {
-    mockLineServiceFind.mockResolvedValue({
-      userId: "testUserId",
-      channelAccessToken: "testChannelAccessToken",
-    });
-    mockSendMessage.mockRejectedValue(new Error());
-
     await expect(sendTestNotification("testMessage")).resolves.toEqual(
-      "failure"
+      "systemError"
     );
+    expect(mockSendMessage).not.toHaveBeenCalled();
   });
 });
