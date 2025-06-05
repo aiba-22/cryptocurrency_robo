380823370d9b7bbb65b1a39dd24b05aa99e7b137
aiba <jazzarubamu@gmail.com>
2025-06-05 14:17:32 +0900

    間隔値変更

--- backend/src/batch.ts
+++ backend/src/batch.ts
@@ -2,5 +2,5 @@ import cron from "node-cron";
 import { autoOrder } from "./useCase/autoOrder";
 import { autoAlert } from "./useCase/autoAlert";
 
-cron.schedule("*/5 * * * *", autoAlert);
-cron.schedule("*/5 * * * *", autoOrder);
+cron.schedule("*/30 * * * *", autoAlert);
+cron.schedule("*/30 * * * *", autoOrder);
